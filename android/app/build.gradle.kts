c67abd344b7571204ec75d624c0610be83cf6258
Rodney <rodmandgrp@gmail.com>
2026-02-25 22:31:34 +0100

    Update app icon

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.pathway_jobs"
-        minSdk = 21
+        minSdk = flutter.minSdkVersion
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
